345a105bb684ef6dacacf9a3c0b08e61098d5459
MSI\yumai <yuma032217@gmail.com>
2021-09-06 20:00:50 +0900

    新モック ・プレイヤー動き ・隕石動き

--- Assets/Scripts/MeteoriteController.cs
+++ Assets/Scripts/MeteoriteController.cs
@@ -5,25 +5,39 @@ using UnityEngine;
 public class MeteoriteController : MonoBehaviour
 {
     // Start is called before the first frame update
-    float move_speed;
+    
+    public static float move_speed = 100.0f;
 
     GameObject player;
+
+    Rigidbody r_body;
     void Start()
     {
         //move_speed = 0.1f + 0.1f * Random.value;
+        r_body = GetComponent<Rigidbody>();
 
         player = GameObject.FindGameObjectWithTag("Player");
+
+        Vector3 velocity = new Vector3(0.0f, 0.0f, -move_speed);
+        r_body.velocity = velocity;
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+
         //transform.Translate(0.0f, 0.0f, -move_speed);
 
-        //if (transform.position.z < -10.0f)
-        //{
-        //    Destroy(gameObject);
-        //}
+        if (transform.position.z < -10.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        
     }
 
     //private void OnCollisionEnter(Collision collision)
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,25 +7,42 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
 
     [SerializeField]
-    float player_speed = 1.0f;
-
-    [SerializeField]
-    float player_side_add_speed = 1.0f;
+    float player_speed = 50.0f;
 
     [SerializeField]
     float player_max_speed = 10.0f;
 
     [SerializeField]
-    float player_x_speed = 30.0f;
+    float meteo_add_speed = 10.0f;
 
     [SerializeField]
-    float player_hit_heal_seconds = 2.0f;
+    float meteo_max_speed = 300.0f;
 
     [SerializeField]
-    float player_hit_heal_speed = 1000.0f;
+    float invincible_time = 2.0f;
 
     [SerializeField]
-    float dashbord_add_speed = 2500.0f;
+    float meteo_add_max_time = 1.5f;
+
+    //[SerializeField]
+    //float player_speed = 1.0f;
+
+    //[SerializeField]
+    //float player_side_add_speed = 1.0f;
+
+
+
+    //[SerializeField]
+    //float player_x_speed = 30.0f;
+
+    //[SerializeField]
+    //float player_hit_heal_seconds = 2.0f;
+
+    //[SerializeField]
+    //float player_hit_heal_speed = 1000.0f;
+
+    //[SerializeField]
+    //float dashbord_add_speed = 2500.0f;
 
     [SerializeField]
     PhysicMaterial physicMaterial;
@@ -33,124 +50,281 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     float player_inertia = 0.7f;
 
-    public GameObject player;
+    public GameObject player_box;
 
     Rigidbody r_body;
-    bool leftFlag, rightFlag;
+    Collider player_hit_box;
+    bool leftFlag, rightFlag, upFlag, downFlag;
 
     float maxSpeed;
     float stay_time;
     bool side_hit_flag;
     bool right_side_flg;
     bool left_side_flg;
+    float stick_angle;
+
+    bool meteo_add_flg;
+    bool meteo_gensoku_flg;
+    float gensokuTime;
+    float nowSpeed;
+    bool meteo_hit_flg;
+    bool meteo_active;
+
 
     void Start()
     {
         leftFlag  = false;
         rightFlag = false;
+        upFlag = false;
+        downFlag = false;
         side_hit_flag = false;
         right_side_flg = false;
         left_side_flg = false;
+        meteo_add_flg = false;
+
+        r_body = GetComponent<Rigidbody>();
+
+        player_hit_box = GetComponent<Collider>();
+
+        
     }
 
     private void FixedUpdate()
     {
-        r_body = GetComponent<Rigidbody>();
-        Vector3 force = gameObject.transform.rotation * new Vector3(0.0f, 0.0f, 5.0f);
-//        r_body.AddForce(force);
-
-        if (rightFlag)
+        if (meteo_hit_flg)
         {
-            if (r_body.velocity.x < 0.0f)
+            if (!meteo_active)
             {
-                var velocity = r_body.velocity;
-                velocity.x *= player_inertia;
-                r_body.velocity = velocity;
+                meteo_active = true;
+
+                if (meteo_active)
+                {
+                    player_box.SetActive(true);
+                }
             }
+            else
+            {
+                meteo_active = false;
 
-            //            r_body.AddForce(player_x_speed, 0.0f, 0.0f);
-            force += new Vector3(player_x_speed, 0.0f, 0.0f);
-            rightFlag = false;
+                if (!meteo_active)
+                {
+                    player_box.SetActive(false);
+                }
+            }
         }
-
-        if (leftFlag)
+        else
         {
-            if (r_body.velocity.x > 0.0f)
-            {
-                var velocity = r_body.velocity;
-                velocity.x *= player_inertia;
-                r_body.velocity = velocity;
-            }
-
-            //r_body.AddForce(-player_x_speed, 0.0f, 0.0f);
-            force += new Vector3(-player_x_speed, 0.0f, 0.0f);
-            leftFlag = false;
+            player_box.SetActive(true);
         }
+        //Vector3 force = gameObject.transform.rotation * new Vector3(0.0f, 0.0f, 5.0f);
+        //        r_body.AddForce(force);
 
-        
+        //if (rightFlag)
+        //{
+        //    if (r_body.velocity.x < 0.0f)
+        //    {
+        //        var velocity = r_body.velocity;
+        //        velocity.x *= player_inertia;
+        //        r_body.velocity = velocity;
+        //    }
+
+        //    r_body.AddForce(player_x_speed, 0.0f, 0.0f);
+        //    //force += new Vector3(player_x_speed, 0.0f, 0.0f);
+        //    rightFlag = false;
+        //}
+
+        //if (leftFlag)
+        //{
+        //    if (r_body.velocity.x > 0.0f)
+        //    {
+        //        var velocity = r_body.velocity;
+        //        velocity.x *= player_inertia;
+        //        r_body.velocity = velocity;
+        //    }
+
+        //    r_body.AddForce(-player_x_speed, 0.0f, 0.0f);
+        //    //force += new Vector3(-player_x_speed, 0.0f, 0.0f);
+        //    leftFlag = false;
+        //}
+
+        //if (upFlag)
+        //{
+        //    if (r_body.velocity.z < 0.0f)
+        //    {
+        //        var velocity = r_body.velocity;
+        //        velocity.z *= player_inertia;
+        //        r_body.velocity = velocity;
+        //    }
+
+        //    r_body.AddForce(0.0f, 0.0f, player_x_speed);
+        //    //force += new Vector3(player_x_speed, 0.0f, 0.0f);
+        //    upFlag = false;
+        //}
+
+        //if (downFlag)
+        //{
+        //    if (r_body.velocity.z > 0.0f)
+        //    {
+        //        var velocity = r_body.velocity;
+        //        velocity.z *= player_inertia;
+        //        r_body.velocity = velocity;
+        //    }
+
+        //    r_body.AddForce(0.0f, 0.0f, -player_x_speed);
+        //    //force += new Vector3(player_x_speed, 0.0f, 0.0f);
+        //    downFlag = false;
+        //}
 
         if (r_body.velocity.magnitude > player_max_speed)
         {
             r_body.velocity = r_body.velocity.normalized * player_max_speed;
         }
 
-        //if (r_body.velocity.magnitude < limit_left_speed)
-        //{
-        //    r_body.velocity = r_body.velocity.normalized * limit_left_speed;
-        //}
-
-        //transform.position += force * Time.deltaTime;
-        r_body.AddForce(force);
+        //r_body.AddForce(force);
     }
 
     // Update is called once per frame
     void Update()
     {
-        float d_pad_h = Input.GetAxis("D_Pad_H");
         float l_stick_h = Input.GetAxis("L_Stick_H");
+        float l_stick_v = Input.GetAxis("L_Stick_V");
 
-        if (!right_side_flg)
+        Vector3 velocity;
+        velocity.x = l_stick_h * Mathf.Sqrt(1.0f - 0.5f * l_stick_v * l_stick_v);
+        velocity.y = 0.0f;
+        velocity.z = l_stick_v * Mathf.Sqrt(1.0f - 0.5f * l_stick_h * l_stick_h);
+
+        r_body.velocity = velocity * player_speed;
+
+        if (!meteo_hit_flg)
         {
-            if (Input.GetKey(KeyCode.D) || d_pad_h > 0 || l_stick_h > 0)
+            if (Input.GetKey("joystick button 1"))
             {
-                rightFlag = true;
-                //            transform.Rotate(0, 50 * Time.deltaTime, 0);
+                meteo_add_flg = true;
+                meteo_gensoku_flg = false;
+
+                Vector3 meteo_velocity = new Vector3(0.0f, 0.0f, -meteo_add_speed);
+                GameObject[] meteos = GameObject.FindGameObjectsWithTag("Meteo Tag");
+                for (int i = 0; i < meteos.Length; i++)
+                {
+                    var v = meteos[i].GetComponent<Rigidbody>().velocity;
+                    v += meteo_velocity;
+
+                    if (v.z < -meteo_max_speed)
+                    {
+                        v.z = -meteo_max_speed;
+                    }
+                    meteos[i].GetComponent<Rigidbody>().velocity = v;
+                }
+
+                if (meteos.Length > 0)
+                    nowSpeed = meteos[0].GetComponent<Rigidbody>().velocity.z;
+            }
+
+            if (meteo_add_flg)
+            {
+                if (Input.GetKeyUp("joystick button 1"))
+                {
+                    meteo_add_flg = false;
+                    meteo_gensoku_flg = true;
+                    gensokuTime = 0.0f;
+                }
             }
         }
-
-        if (!left_side_flg)
-        {
-            if (Input.GetKey(KeyCode.A) || d_pad_h < 0 || l_stick_h < 0)
-            {
-                leftFlag = true;
-                //            transform.Rotate(0, -50 * Time.deltaTime, 0);
-            }
-        }
-        
-
-        if (r_body.velocity.z >= 20.0f)
-            physicMaterial.bounciness = 0.2f;
         else
-            physicMaterial.bounciness = 0.4f;
+        {
+            meteo_gensoku_flg = true;
+        }
 
-        if (r_body.velocity.z > maxSpeed)
-            maxSpeed = r_body.velocity.z;
+        if (meteo_gensoku_flg)
+        {
+            gensokuTime += Time.deltaTime * meteo_add_max_time;
+            GameObject[] meteos = GameObject.FindGameObjectsWithTag("Meteo Tag");
+            for (int i = 0; i < meteos.Length; i++)
+            {
+                float speed = Mathf.Lerp(nowSpeed, -MeteoriteController.move_speed, gensokuTime);
+                meteos[i].GetComponent<Rigidbody>().velocity = new Vector3(0, 0, speed);
+            }
+
+            if (gensokuTime >= 1.0f)
+                meteo_gensoku_flg = false;
+        }
+
+        //float d_pad_h = Input.GetAxis("D_Pad_H");
+        //float d_pad_v = Input.GetAxis("D_Pad_V");
+        //float l_stick_h = Input.GetAxis("L_Stick_H");
+        //float l_stick_v = Input.GetAxis("L_Stick_V");
+
+        //if (!right_side_flg)
+        //{
+        //    if (Input.GetKey(KeyCode.D) || d_pad_h > 0 || l_stick_h > 0)
+        //    {
+        //        rightFlag = true;
+        //        //            transform.Rotate(0, 50 * Time.deltaTime, 0);
+        //    }
+        //}
+
+        //if (!left_side_flg)
+        //{
+        //    if (Input.GetKey(KeyCode.A) || d_pad_h < 0 || l_stick_h < 0)
+        //    {
+        //        leftFlag = true;
+        //        //            transform.Rotate(0, -50 * Time.deltaTime, 0);
+        //    }
+        //}
+
+        //if (Input.GetKey(KeyCode.W) || d_pad_v > 0 || l_stick_v > 0)
+        //{
+        //    upFlag = true;
+        //    //            transform.Rotate(0, 50 * Time.deltaTime, 0);
+        //}
+
+        //if (Input.GetKey(KeyCode.S) || d_pad_v < 0 || l_stick_v < 0)
+        //{
+        //    downFlag = true;
+        //    //            transform.Rotate(0, 50 * Time.deltaTime, 0);
+        //}
+
+
+        //if (r_body.velocity.z >= 20.0f)
+        //    physicMaterial.bounciness = 0.2f;
+        //else
+        //    physicMaterial.bounciness = 0.4f;
+
+        //if (r_body.velocity.z > maxSpeed)
+        //    maxSpeed = r_body.velocity.z;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Front"))
+        if (!meteo_hit_flg)
         {
-            Invoke("AddPower", player_hit_heal_seconds);
+            if (collision.gameObject.CompareTag("Meteo Tag"))
+            {
+                this.gameObject.layer = LayerMask.NameToLayer("Invincible");
+                Invoke("StopInvincible", invincible_time);
+                meteo_hit_flg = true;
+            }
         }
+        //if (collision.gameObject.CompareTag("Front"))
+        //{
+        //    Invoke("AddPower", player_hit_heal_seconds);
+        //}
 
-        if (collision.gameObject.CompareTag("Side"))
-        {
-            side_hit_flag = true;
-        }
+        //if (collision.gameObject.CompareTag("Side"))
+        //{
+        //    side_hit_flag = true;
+        //}
 
         
     }
+
+    void StopInvincible()
+    {
+        this.gameObject.layer = LayerMask.NameToLayer("Default");
+        meteo_hit_flg = false;
+    }
+
     private void OnCollisionExit(Collision collision)
     {
 
@@ -161,16 +335,12 @@ public class PlayerController : MonoBehaviour
 
     }
 
-    void AddPower()
-    {
-        r_body.AddForce(0.0f, 0.0f, player_hit_heal_speed);
-        maxSpeed = 0.0f;
-    }
+    
 
     private void OnTriggerEnter(Collider other)
     {
-        stay_time = 0.0f;
-        side_hit_flag = false;
+        //stay_time = 0.0f;
+        //side_hit_flag = false;
 
         if (other.gameObject.CompareTag("Right Side"))
         {
@@ -185,15 +355,15 @@ public class PlayerController : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-        stay_time += Time.deltaTime;
+        //stay_time += Time.deltaTime;
     }
 
     private void OnTriggerExit(Collider other)
     {      
-        if (!side_hit_flag)
-        r_body.AddForce(0.0f, 0.0f, player_side_add_speed * stay_time);
+        //if (!side_hit_flag)
+        //r_body.AddForce(0.0f, 0.0f, player_side_add_speed * stay_time);
 
-        side_hit_flag = false;
+        //side_hit_flag = false;
         right_side_flg = false;
         left_side_flg = false;
     }
